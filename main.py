60d90db2a95263f89041e1f6ecf3ae36facf1e43
Minh Nguyen <121152335+Minh111104@users.noreply.github.com>
2024-09-14 23:58:37 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+from turtle import Screen
+import time  # slow the turtle down
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)  # turn off tracer
+
+game_is_on = True
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    # trigger the snake class
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        # extend the snake
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        # game_is_on = False
+        # scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
+    # # detect collision with tail
+    # for segment in snake.segments:
+        # # pass the first segment
+        # if segment == snake.head:
+        #     pass
+        # elif snake.head.distance(segment) < 10:
+        #     game_is_on = False
+        #     scoreboard.game_over()
+
+    # # Using Slice list method
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            # game_is_on = False
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
+
+screen.exitonclick()
+
+# segment_1 = Turtle(shape="square")
+# segment_1.color("white")
+# segment_1.goto(x=0, y=0)
+#
+# segment_2 = Turtle(shape="square")
+# segment_2.color("white")
+# segment_2.goto(x=-20, y=0)
+#
+# segment_3 = Turtle(shape="square")
+# segment_3.color("white")
+# segment_3.goto(x=-40, y=0)
